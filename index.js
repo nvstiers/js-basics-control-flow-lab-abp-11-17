37031a76eba8af148e52ec23019eaaa58d6b2c87
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 18:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet(distance) {
-  if (distance <== 400) {
+function scuberGreetingForFeet(feet) {
+  if (feet <== 400) {
     resturn 'This one is on me!';
   }
 }
