398ca8a67a5895af56e8ae59647a24c5a3e407d4
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 19:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(ride) {
 }
 
 function ternaryCheckCity(city) {
-  (city === 'NYC') ? 'Ok, sounds good.' : 'No go.' ;
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.' ;
 }
