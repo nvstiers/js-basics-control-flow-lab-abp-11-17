9e85dad420098ec2c2219bb76287f5372ab47b41
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 19:19:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
     return 'This one is on me!';
   }
-  else if (ride >=2501) {
-    return 'No can do.';
-  }
   else if (ride > 2000) {
     return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride >=2501) {
+    return 'No can do.';
   }
 }
 
