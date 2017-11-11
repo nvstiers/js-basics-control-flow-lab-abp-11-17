9255554dc28d33a7817d93a6dbd23114d46cdbc3
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 19:35:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  }
+  else if (2000<ride && ride< 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride >=2501) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.' ;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much." ;
+      break;
+    case 'not as generous':
+        return 'Thank you.' ;
+      break;
+      default:
+      return "Bye." ;
+  }
+}
